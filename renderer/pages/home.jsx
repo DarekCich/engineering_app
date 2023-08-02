03a91370a996e135eb8d8c3a31a25c07cc90fe2f
DarekCich <darek.c65@gmail.com>
2023-08-02 23:01:21 +0200

    Podstawy interfejsu i responsywność

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -10,14 +10,17 @@ function Home() {
       </Head>
       <div className={styles.homePage}>
         <div className={styles.bar}>
-
         </div>
         <div className={styles.content}>
           <div className={styles.folderSite}>
-          
           </div>
           <div className={styles.itemsSite}>
-            
+            <div className={styles.fileList}>
+            </div>
+            <div className={styles.fileDetails}>
+              <div className={styles.fileView}></div>
+              <div className={styles.fileDetail}></div>
+            </div>
           </div>
         </div>
       </div>
