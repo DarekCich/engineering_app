f83453f23ca7f0f0a664ea54ab7d41c07b86c384
DarekCich <darek.c65@gmail.com>
2023-08-04 23:20:36 +0200

    first buttons

---
+++ renderer/components/folders.jsx
@@ -0,0 +1,4 @@
+function folderBar(){
+
+}
+export default folderBar;
--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -14,6 +14,18 @@ function Home() {
         </div>
         <div className={styles.content}>
           <div className={styles.folderSite}>
+            <div className={styles.folderBar}>
+              <div className={styles.folderName}>Lista Folderów</div>
+              <div className={styles.folderButtons}>
+                <button className={styles.folderButton}>
+                  <img src='/images/reload.png' className={styles.iconsFolderButtons}></img>
+                </button>
+                <button className={styles.folderButton}>
+                  <img src='/images/createFolder.png' className={styles.iconsFolderButtons}></img>
+                </button>
+                
+              </div>
+            </div>
           </div>
           <ItemsSite/>
         </div>
